47c882481d6a9cc5c5b8ab06922ef55ad199ce90
LukaszCheciak <92047253+LukaszCheciak@users.noreply.github.com>
2021-11-23 23:03:46 +0100

    Delete 19.py

--- 19.py
+++
@@ -1,8 +0,0 @@
-print("podaj dłogości boków trójkąta: ")
-a = float(input())
-b = float(input())
-c = float(input())
-p = (a+b+c)/2
-P = (p*(p-a)*(p-b)*(p-c))**(1/2)
-print("Pole trójkąta wynosi {}".format(P))
-
