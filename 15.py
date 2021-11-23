e7b10127584fd01adb41751ef3cfd0be15ae6203
LukaszCheciak <92047253+LukaszCheciak@users.noreply.github.com>
2021-11-23 23:03:35 +0100

    Delete 15.py

--- 15.py
+++
@@ -1,6 +0,0 @@
-print("Jaka jest temperatura w stopniach Celsjusza ?")
-temp = float(input())
-K = temp + 273
-F = 9/5 * temp + 32
-print("to jest {} stopni Fahrenheita".format(F))
-print("to jest {} stopni Kelwina".format(K))
